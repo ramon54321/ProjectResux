8a440b68b28d16efa4e52d0aff953ee1e77a6ca8
Ramon Brand <ramon2009@gmail.com>
2020-01-29 22:42:25 +0200

    Implement initial Path2D

---
+++ js/src/main/scala/sux/client/rendering/extensions/Path2D.scala
@@ -0,0 +1,11 @@
+package sux.client.rendering.extensions
+
+import scala.scalajs.js
+import scala.scalajs.js.annotation.JSGlobal
+
+@js.native
+@JSGlobal
+class Path2D extends js.Object {
+  def this(d: String) = this()
+  def this(path: Path2D) = this()
+}
