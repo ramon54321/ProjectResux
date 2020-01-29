31c21adbad7047f959eb2f9ad18e64b9d79a9852
Ramon Brand <ramon2009@gmail.com>
2020-01-29 21:52:00 +0200

    Add timezone to config injected build date

--- build-client.sh
+++ build-client.sh
@@ -37,7 +37,7 @@ source project.config
 COMMIT_HASH=$(git log --pretty=format:"%h" HEAD^1..HEAD)
 COMMIT_AUTHOR=$(git log --pretty=format:"%cn" HEAD^1..HEAD)
 CLIENT_BUILD_HASH=$(shasum ./build/client/sux_client.js | awk '{split($0,a,"  "); print a[1]}')
-BUILD_DATE=$(date '+%d.%m.%Y %H:%M')
+BUILD_DATE=$(date '+%d.%m.%Y %H:%M %Z')
 sed -i '' "3i\\
 \\ \\ version: \"${VERSION}\",\\
 \\ \\ commitHash: \"${COMMIT_HASH}\",\\
