be5935596fb6f5830ae7e64f03f0476cc128052f
Ramon Brand <ramon2009@gmail.com>
2020-02-09 16:46:24 +0200

    Use dispatchTo for full state update

--- jvm/src/main/scala/sux/server/game/ClientActionHandler.scala
+++ jvm/src/main/scala/sux/server/game/ClientActionHandler.scala
@@ -8,6 +8,6 @@ import sux.server.Hub
 object ClientActionHandler {
   def handleClientAction(clientAction: ClientAction, webSocket: WebSocket): Unit = clientAction match {
     case ClientActions.Ping(timestamp) => Hub.dispatchTo(WorldActions.Ping(timestamp), webSocket)
-    case ClientActions.FullStateUpdate() => Hub.worldActionHistory.foreach(worldAction => webSocket.send(WorldActions.Serializer.toJson(worldAction)))
+    case ClientActions.FullStateUpdate() => Hub.worldActionHistory.foreach(worldAction => Hub.dispatchTo(worldAction, webSocket))
   }
 }
