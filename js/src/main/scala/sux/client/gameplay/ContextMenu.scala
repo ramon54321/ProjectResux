996bff5db0317901fe34efc8d47e7376d3a0d605
Ramon Brand <ramon2009@gmail.com>
2020-02-29 17:00:30 +0200

    Fix context menu target position

--- js/src/main/scala/sux/client/gameplay/ContextMenu.scala
+++ js/src/main/scala/sux/client/gameplay/ContextMenu.scala
@@ -1,16 +1,18 @@
 package sux.client.gameplay
 
+import sux.client.rendering.{FrameInfo, Mapping}
 import sux.client.{ContextMenuNode, InterfaceState}
 import sux.common.enums.Enums.MoveSpeed
+import sux.common.math.Vec2F
 
 object ContextMenu {
   def getContextMenu: Either[String, ContextMenuNode] = {
     val selectedEntity = InterfaceState.getSelectedEntity
     if (selectedEntity.isDefined) {
       Right(createContextMenu(
-        new ContextMenuNode("Slow Move", frameInfo => Orchestration.moveEntity(selectedEntity.get, frameInfo.mouseWorldPosition, MoveSpeed.Slow)),
-        new ContextMenuNode("Move", frameInfo => Orchestration.moveEntity(selectedEntity.get, frameInfo.mouseWorldPosition, MoveSpeed.Normal)),
-        new ContextMenuNode("Fast Move", frameInfo => Orchestration.moveEntity(selectedEntity.get, frameInfo.mouseWorldPosition, MoveSpeed.Fast)),
+        new ContextMenuNode("Slow Move", frameInfo => Orchestration.moveEntity(selectedEntity.get, getContextMenuTargetPosition(frameInfo), MoveSpeed.Slow)),
+        new ContextMenuNode("Move", frameInfo => Orchestration.moveEntity(selectedEntity.get, getContextMenuTargetPosition(frameInfo), MoveSpeed.Normal)),
+        new ContextMenuNode("Fast Move", frameInfo => Orchestration.moveEntity(selectedEntity.get, getContextMenuTargetPosition(frameInfo), MoveSpeed.Fast)),
         new ContextMenuNode("Stop", frameInfo => Orchestration.stopEntity(selectedEntity.get)),
         new ContextMenuNode("Debug", _ => Unit,
           new ContextMenuNode("Entity", frameInfo => println(selectedEntity.get.toString(frameInfo.worldTime))),
@@ -23,5 +25,6 @@ object ContextMenu {
     }
   }
 
+  private def getContextMenuTargetPosition(frameInfo: FrameInfo): Vec2F = Mapping.canvasSpaceToWorldSpace(frameInfo, InterfaceState.getContextMenuCanvasCenter)
   private def createContextMenu(options: ContextMenuNode*) = new ContextMenuNode("Root", _ => Unit, options:_*)
 }
